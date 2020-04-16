a242cf54df6bce2de805714ce5b3426317a66661
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 19:39:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1 #returns array to string
 elsif array.length == 2
   array.join("and") #returns array with a string "and"
   
-elseif array.length == 3
+else array.length => 3
   return array.join("and") #returns 3 element array with "," "and"
 end
 end
