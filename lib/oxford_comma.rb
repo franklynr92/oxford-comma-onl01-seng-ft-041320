336229a7ba4ac12212f37d8cf2bbc6f0f8bb6c9f
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 19:54:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1 #returns array to string
     array[0]
 elsif array.length == 2
   array.join(" and ") 
-elsif array.length > 2
+elsif array.length >= 3
   last_element = array.pop
    almost_done = array.join(",") 
    almost_done + ", and " + last_element
