9d1e9eb616def2779449d102f2076c6e1e0c3503
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 19:33:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.length == 1 #returns array to string
     array
 elsif array.length == 2
-  array.unshift("and") #returns array with a string "and"
+  array.join("and") #returns array with a string "and"
   
 else
   return array.join("and") #returns 3 element array with "," "and"
